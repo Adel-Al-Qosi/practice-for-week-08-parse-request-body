aeb4a49216e531e85505f897d1b3f479192688dc
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-31 10:11:46 +0300

    finish practice

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,40 @@
 function firstStep(input) {
   // Your code here
+  return input.split('&')
 }
 
 function secondStep(input) {
   // Your code here
+  return input.map(string => string.split('='))
 }
 
 function thirdStep(input) {
   // Your code here
+  return input.map(array => array.map(string => string.replace(/[+]/i, ' ')))
 }
 
 function fourthStep(input) {
   // Your code here
+  return input.map(arr => arr.map(string => decodeURIComponent(string)))
 }
 
 function fifthStep(input) {
   // Your code here
+  let obj = {
+    
+  }
+  
+  input.forEach(arr => {
+    obj[arr[0]] = arr[1]
+  })
+
+  return obj
 }
 
 function parseBody(str) {
   // Your code here
+  let obj = fifthStep(fourthStep(thirdStep(secondStep(firstStep(str)))))
+  return obj
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
-let server;
+const http = require('http');
+// create the server
+server = http.createServer((req, res) => {
+  // gather up all the data into reqBody
+  let reqBody = '';
+  req.on('data', (data) => {
+    reqBody += data;
+  });
+  // when there's no more data
+  req.on('end', () => {
+    // if we got some data
+    if (reqBody) {
+      // parse the data
+      let parsed = parseBody(reqBody);
+      req.body = parsed;
+    }
+    // send the form page
+    sendFormPage(req, res);
+  });
+});
+
+const port = 5000
+
+server.listen(port, () => console.log('the server is listening to the port ', port))
+
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
